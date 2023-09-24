37fb1f7e2b4d6b789e2610345aac33c1133a3d5e
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-09-24 12:07:12 +0800

    test ci

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,4 @@
 import axios from 'axios';
 export const userRequest = axios.create({
   baseURL: import.meta.env.VITE_APP_BACKEND_BASE_URL || 'http://localhost:9527/',
-  // baseURL: 'https://luo-dian-api-release.meteor.today',
-
-  // headers: {
-  //   Accept: 'application/json',
-  // },
 });
