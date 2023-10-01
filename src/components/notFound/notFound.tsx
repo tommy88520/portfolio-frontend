a06eaab7a5936b5c9b5c310ef65fa95e2087efec
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-10-01 20:36:27 +0800

    fix file name

--- src/components/notFound/notFound.tsx
+++ src/components/notFound/notFound.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import './NotFound.scss';
+import './notFound.scss';
 
 const NotFound = () => {
   const navigate = useNavigate();
